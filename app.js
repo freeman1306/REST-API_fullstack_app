1197f29508964988976528f66e3339d5d1ca6f37
freeman1306 <zavalkin06@gmail.com>
2020-07-22 01:17:30 +0300

    done

--- app.js
+++ app.js
@@ -1,15 +1,56 @@
 const express = require('express')
 const path = require('path')
-
-
+const {v4} = require('uuid')
 // Create server, can use its methods
 const app = express()
+
+
+// our DB
+let CONTACTS = [
+    {id: v4(), name: 'Vladilen', value: '+7-921-100-20-30', marked: false}
+]
+
+
+app.use(express.json())
+
 
 // Open server on port
 app.listen(4500, console.log('Server has been started'))
 
 // Specify static folder
 app.use(express.static(path.resolve(__dirname, 'client')))
+
+// GET
+app.get('/api/contacts', (req, res) => {
+    setTimeout(() => {
+        res.status(200).json(CONTACTS)
+   }, 1000)
+})
+
+
+// POST
+app.post('/api/contacts', (req, res) => {
+   const contact = {...req.body, id: v4(), marked: false}
+    CONTACTS.push(contact)
+    res.status(201).json(contact)
+})
+
+
+// DELETE
+app.delete('/api/contacts/:id', (req, res) => {
+    CONTACTS = CONTACTS.filter(c => c.id !== req.params.id)
+    res.status(200).json({message: 'контакт удален'})
+})
+
+
+// PUT
+app.put('/api/contacts/:id', (req, res) => { 
+    const idx = CONTACTS.findIndex(c => c.id === req.params.id)
+    CONTACTS[idx] = req.body
+    res.json(CONTACTS[idx])
+})
+
+
 
 // Say for all get queries send html from client folder
 app.get('*', (req, res) => {
--- client/frontend.js
+++ client/frontend.js
@@ -1,5 +1,15 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
 
+
+Vue.component('loader', {
+    template: ` <div style='display: flex; justify-content: center; align-items: center;'>
+            <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>`
+})
+
+
 new Vue({
 
     // Initialize Vue app by id
@@ -8,12 +18,13 @@ new Vue({
     // Create data model for connecting with DOM
     data() {
         return {
+            loading: false,
             form: {
                 name: '',
                 value: ''
             },
             contacts: [
-                {id: 1, name: 'Vladilen', value: '+7-921-100-20-30', marked: false}
+
             ]
         }
     },
@@ -25,20 +36,60 @@ new Vue({
 
     // Create methods for sending in DOM/components
     methods: {
-        createContact() {
+        async createContact() {
             const {
                 ...contact
             } = this.form
-           this.contacts.push({...contact, id: Date.now(), marked: false})
 
-            this.form.name = this.form.value = ''
+            const newContact = await request('/api/contacts', 'POST', contact)
+            this.contacts.push(
+                newContact
+            )
+
+            // this.form.name = this.form.value = ''
         },
-        markContact(id) {
-            const contact = this.contacts.find(c => c.id === id)
-            contact.marked = true
+       async markContact(id) {
+           const contact = this.contacts.find(c => c.id === id)
+           const updated = await request(`/api/contacts/${id}`, 'PUT', {
+               ...contact,
+               marked: true
+           })
+            contact.marked = updated.marked
         },
-        removeContact(id) {
-            this.contacts = this.contacts.filter(c => c.id !== id)
+        async removeContact(id) {
+            await request(`/api/contacts/${id}`, 'DELETE')
+            this.contacts = this.contacts.filter(arr => arr.id !== id)
+
+            // this.contacts = this.contacts.filter(c => c.id !== id)
         }
     },
+    async mounted() {
+        this.loading = true
+        this.contacts = await request('/api/contacts')
+        this.loading = false
+    }
 })
+
+
+
+
+async function request(url, method = 'GET', data = null) {
+    try {
+        const headers = {}
+        let body
+
+        if (data) {
+            headers['Content-Type'] = 'application/json'
+            body = JSON.stringify(data)
+        }
+
+        const response = await fetch(url, {
+            method,
+            headers,
+            body
+        })
+        return await response.json()
+    } catch (error) {
+        console.warn('Error: ', error.message)
+    }
+}
